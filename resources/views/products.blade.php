abc013d1596e44ec2d712310a9d4b2d736168816
Leen-Alattar <90185019+Leen-Alattar@users.noreply.github.com>
2021-12-16 16:02:59 +0200

    first repo

---
+++ resources/views/products.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>NAME</th>
+            <th>Price</th>
+        </tr>
+        <tr>
+            <td>{{$id}}</td>
+            <td>{{$name}}</td>
+            <td>{{$price}}</td>
+        </tr>
+
+    </table>
+
+</body>
+
+</html>
